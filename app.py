17304bddbe1e342106db0478c35464b1c3913e02
Cesar98BC <tjemarcus98@gmail.com>
2024-12-12 07:55:16 -0600

    correcion

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     multi1=2344
-    pro=multi1/2
-    return render_template('index.html',multi1=multi1,pro=pro)
+    multi2=3756
+    multi3=5868
+    sum=multi1 + multi2 + multi3
+    pro=sum/3
+    return render_template('index.html',multi1=multi1,multi2=multi2,multi3=multi3,pro=pro)
